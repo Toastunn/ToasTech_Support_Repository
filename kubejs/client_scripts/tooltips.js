fc0f0a0d78b9ef88f1a7c7bacb1e78ba05dca87b
Toastuun <t0ast.ers69420@gmail.com>
2023-12-03 21:22:16 -0500

    tooltips work consistently now!

--- kubejs/client_scripts/tooltips.js
+++ kubejs/client_scripts/tooltips.js
@@ -1,20 +1,42 @@
-//client_scripts.lang.js
+global.itemTooltips = []
 const $ItemDescription = Java.loadClass('com.simibubi.create.foundation.item.ItemDescription$Modifier');
 const $TooltipModifier = Java.loadClass('com.simibubi.create.foundation.item.TooltipModifier');
 const $Palette = Java.loadClass('com.simibubi.create.foundation.item.TooltipHelper$Palette');
 ClientEvents.lang('en_us',event => {
     global.itemTooltips.forEach(tooltipObject => {
-        let [stack, tooltip] = tooltipObject
-        console.info(`adding ${tooltip} to ${stack}`)
-        event.add(`${stack.item.getDescriptionId()}.tooltip.summary`, tooltip)
-        global.itemTooltips = global.itemTooltips.filter(item => item !== tooltipObject)
+        let [id, tooltip] = tooltipObject
+        let item = Item.of(id)
+        console.info(`adding ${tooltip} to ${item}`)
+        event.add(`${item.getDescriptionId()}.tooltip.summary`, tooltip)
     });
-    Ingredient.all.stacks.forEach(stack => {
-        $TooltipModifier.REGISTRY.register(
-            stack.item.id, new $ItemDescription(
-                stack.item,
-                $Palette.STANDARD_CREATE
-            )
+    Ingredient.all.itemIds.forEach(id => {
+        $TooltipModifier.REGISTRY.registerDeferred(
+          id, (item) => new $ItemDescription(
+              item,
+              $Palette.STANDARD_CREATE
+          )
         )
     });
 });
+/*In kubejs/assets/kubejs/lang/en_us.js, use the following format;
+for blocks;
+    "block.modid.itemid.tooltip": "EXAMPLE ITEM (just a marker that this tooltip exists)",
+    "block.modid.itemid.tooltip.summary": "A brief description of the item. _Underscores_ highlight a term.",
+    "block.modid.itemid.tooltip.condition1": "When this",
+    "block.modid.itemid.tooltip.behaviour1": "Then this item does this. (behaviours show on shift)",
+    "block.modid.itemid.tooltip.condition2": "And When this",
+    "block.modid.itemid.tooltip.behaviour2": "You can add as many behaviours as you like",
+    "block.modid.itemid.tooltip.control1": "When Ctrl pressed",
+    "block.modid.itemid.tooltip.action1": "These controls are displayed.",
+
+for items;
+
+    "item.create.example_item.tooltip": "EXAMPLE ITEM (just a marker that this tooltip exists)",
+    "item.create.example_item.tooltip.summary": "A brief description of the item. _Underscores_ highlight a term.",
+    "item.create.example_item.tooltip.condition1": "When this",
+    "item.create.example_item.tooltip.behaviour1": "Then this item does this. (behaviours show on shift)",
+    "item.create.example_item.tooltip.condition2": "And When this",
+    "item.create.example_item.tooltip.behaviour2": "You can add as many behaviours as you like",
+    "item.create.example_item.tooltip.control1": "When Ctrl pressed",
+    "item.create.example_item.tooltip.action1": "These controls are displayed.",
+*/
