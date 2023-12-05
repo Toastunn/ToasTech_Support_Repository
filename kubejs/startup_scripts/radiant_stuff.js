4b9aebdf479fc287d22e5d7a12948b68a4433322
Toastuun <t0ast.ers69420@gmail.com>
2023-12-04 23:04:44 -0500

    new incomplete seq assem  item file

--- kubejs/startup_scripts/radiant_stuff.js
+++ kubejs/startup_scripts/radiant_stuff.js
@@ -3,8 +3,6 @@ StartupEvents.registry('item', e => {
     e.create('radiant_catalyst').displayName("Radiant Catalyst").burnTime(2000)
     //heart of the sun
     e.create('heart_of_a_star').displayName("Heart of a Star")
-    //Radiant alloy
-    e.create('radiant_alloy').displayName("Radiant Alloy")
     //The Sun
     e.create('star').displayName("Star").burnTime(100000000)
 })
---
+++ kubejs/startup_scripts/sequenced_assembly_incomplete_items.js
@@ -0,0 +1,6 @@
+StartupEvents.registry('item', e => {
+    //incomplete ingot of infinity
+    e.create('incomplete_ingot_of_infinity').displayName("Incomplete Ingot of Infinity").rarity('uncommon')
+    //ingots of steel
+    e.create('ingots_of_steel').displayName("Ingots of Steel").tooltip("Serves no purpose outside the assembly line.")
+});
