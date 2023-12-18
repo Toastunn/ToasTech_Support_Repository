a788f416f9fe3daffd7bfc35d736aeace6d28e73
Toastuun <t0ast.ers69420@gmail.com>
2023-12-18 14:46:54 -0500

    log in msg for enderio tooltips

---
+++ kubejs/server_scripts/log_in_messages.js
@@ -0,0 +1,3 @@
+PlayerEvents.loggedIn(event => {
+    event.player.tell(Component.blue('[ToasTech Support Message]: '), Component.white('Reload Textures With '), Component.green('[F3 + T] '), Component.white('to Load EnderIO Tooltips'))
+})
