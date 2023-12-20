c10aee44bb805c14322bd0a9137e37c9d825ba6d
Toastuun <t0ast.ers69420@gmail.com>
2023-12-19 22:17:35 -0500

    new folder for msg when logging in

--- kubejs/server_scripts/log_in_messages.js
+++
@@ -1,3 +0,0 @@
-PlayerEvents.loggedIn(event => {
-    event.player.tell(Component.blue('[ToasTech Support Message]: '), Component.white('Reload Textures With '), Component.green('[F3 + T] '), Component.white('to Load EnderIO Tooltips'))
-})
