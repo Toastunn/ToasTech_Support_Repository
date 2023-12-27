a98b581ae33406a39928c3cfebba77196956368c
Toastuun <t0ast.ers69420@gmail.com>
2023-12-26 22:59:10 -0500

    moved infinite chassis to block creation

--- kubejs/startup_scripts/item_creation/sequenced_assembly_incomplete_items.js
+++ kubejs/startup_scripts/item_creation/sequenced_assembly_incomplete_items.js
@@ -3,6 +3,4 @@ StartupEvents.registry('item', e => {
     e.create('incomplete_ingot_of_infinity').displayName("Incomplete Ingot of Infinity").rarity('uncommon').tooltip("Serves no purpose outside the assembly line.")
     //ingots of steel
     e.create('ingots_of_steel').displayName("Ingots of Steel").tooltip("Serves no purpose outside the assembly line.")
-    //infinite chassis
-    e.create('infinite_chassis').tooltip("Serves no purpose outside the assembly line.")
 });
