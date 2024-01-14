dc7bab627ebe621adb0cf3b8582fe637b2b7b862
Toastuun <t0ast.ers69420@gmail.com>
2024-01-13 20:58:30 -0500

    inscriber doesn't work

--- kubejs/server_scripts/ae2-recipes.js
+++ kubejs/server_scripts/ae2-recipes.js
@@ -1,3 +1,8 @@
+// requires: ae2
 ServerEvents.recipes(event => {
-    
+    event.recipes.ae2.entropy({block: "minecraft:stone_slab[type=bottom]"}, {block: "minecraft:stone_slab[type=top]"}, "heat")
+    event.recipes.ae2.entropy({block: "minecraft:stone_slab[type=top]"}, {block: "minecraft:stone_slab[type=bottom]"}, "cool")
+
+    event.recipes.ae2.transform('extendedcrafting:ender_ingot', ['iron_ingot']).circumstance({type: 'fluid', tag: 'toastech:pulsating_liquid'})
+    event.recipes.ae2.transform('gold_ore', ['stone', 'gold_ingot']).circumstance({type: 'explosion'})
 })
