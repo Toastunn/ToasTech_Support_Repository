b1310bb249e79938cc513ba59e3b3211b5763c9c
Toastuun <t0ast.ers69420@gmail.com>
2024-01-13 20:58:14 -0500

    hiding redstone ingot

---
+++ kubejs/client_scripts/jei_events.js
@@ -0,0 +1,5 @@
+JEIEvents.hideItems(event => {
+  event.hide('extendedcrafting:redstone_ingot')
+  event.hide('extendedcrafting:redstone_nugget')
+  event.hide('extendedcrafting:redstone_ingot_block')
+})
