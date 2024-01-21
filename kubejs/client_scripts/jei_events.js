429e66954b024e13697a80e8fc0473b06d9e6f8a
Toastuun <t0ast.ers69420@gmail.com>
2024-01-20 22:16:50 -0500

    crafting station was being racist

--- kubejs/client_scripts/jei_events.js
+++ kubejs/client_scripts/jei_events.js
@@ -2,4 +2,17 @@ JEIEvents.hideItems(event => {
   event.hide('extendedcrafting:redstone_ingot')
   event.hide('extendedcrafting:redstone_nugget')
   event.hide('extendedcrafting:redstone_ingot_block')
+  event.hide('alltheores:brass_block')
+  event.hide('alltheores:brass_ingot')
+  event.hide('alltheores:brass_nugget')
+  event.hide('alltheores:brass_plate')
+  event.hide('alltheores:brass_gear')
+  event.hide('alltheores:brass_rod')
+  event.hide('alltheores:brass_dust')
+})
+
+JEIEvents.addItems(event => {
+  event.add('extendedcrafting:ultimate_singularity')
+  event.add("craftingstation:crafting_station_slab")
+  event.add("craftingstation:crafting_station")
 })
