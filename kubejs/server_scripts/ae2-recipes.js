5822099530b43c30bb84952d01e0e8eec3d0707f
Toastuun <t0ast.ers69420@gmail.com>
2024-02-04 15:46:24 -0500

    recipes

--- kubejs/server_scripts/ae2-recipes.js
+++ kubejs/server_scripts/ae2-recipes.js
@@ -4,5 +4,4 @@ ServerEvents.recipes(event => {
     event.recipes.ae2.entropy({block: "minecraft:stone_slab[type=top]"}, {block: "minecraft:stone_slab[type=bottom]"}, "cool")
 
     event.recipes.ae2.transform('extendedcrafting:ender_ingot', ['iron_ingot']).circumstance({type: 'fluid', tag: 'toastech:pulsating_liquid'})
-    event.recipes.ae2.transform('gold_ore', ['stone', 'gold_ingot']).circumstance({type: 'explosion'})
 })
