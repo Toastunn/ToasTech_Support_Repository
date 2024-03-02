3593af14f79cbdc86d724398ea7acadbd919fef8
Toastuun <t0ast.ers69420@gmail.com>
2024-03-01 21:52:37 -0500

    glow

--- kubejs/startup_scripts/item_creation/loneliness.js
+++ kubejs/startup_scripts/item_creation/loneliness.js
@@ -1,3 +1,3 @@
 StartupEvents.registry('item', e => {
-    e.create('crystal_of_desolation').displayName("Crystal of Desolation").rarity('uncommon')
+    e.create('crystal_of_desolation').displayName("Crystal of Desolation").rarity('uncommon').glow(true)
 })
