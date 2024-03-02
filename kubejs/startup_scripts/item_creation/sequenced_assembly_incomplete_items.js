2aeca9d076f5fe2e81581b2c40c3e52c0a3b7696
Toastuun <t0ast.ers69420@gmail.com>
2024-03-01 21:53:29 -0500

    2 ingots of steel

--- kubejs/startup_scripts/item_creation/sequenced_assembly_incomplete_items.js
+++ kubejs/startup_scripts/item_creation/sequenced_assembly_incomplete_items.js
@@ -2,5 +2,6 @@ StartupEvents.registry('item', e => {
     //incomplete ingot of infinity
     e.create('incomplete_ingot_of_infinity').displayName("Incomplete Ingot of Infinity").rarity('uncommon').tooltip("Serves no purpose outside the assembly line.")
     //ingots of steel
-    e.create('ingots_of_steel').displayName("Ingots of Steel").tooltip("Serves no purpose outside the assembly line.")
+    e.create('ingots_of_steel_1').displayName("Ingots of Steel").tooltip("Serves no purpose outside the chained assembly line.").texture("kubejs:item/ingots_of_steel")
+    e.create('ingots_of_steel_2').displayName("Ingots of Steel").tooltip("Serves no purpose outside the chained assembly line.").texture("kubejs:item/ingots_of_steel")
 });
