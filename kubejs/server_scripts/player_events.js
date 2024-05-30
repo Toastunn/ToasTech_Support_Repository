59da79db66e5df2443386083ec8a2010b5a1b933
Toastuun <t0ast.ers69420@gmail.com>
2024-05-30 08:05:45 -0400

    player spawn tp

--- kubejs/server_scripts/player_events.js
+++ kubejs/server_scripts/player_events.js
@@ -8,17 +8,17 @@ PlayerEvents.loggedIn(event => {
       player.give(Item.of('patchouli:guide_book', '{"patchouli:book":"extendedcrafting:guide"}'))
       persistentData.gotBooks = true
     }
-    /*if(!persistentData.firstjoin || persistentData.firstjoin == false) {
+    if(!persistentData.firstjoin || persistentData.firstjoin == false) {
       const playerName = player.name.string
       player.runCommandSilent(`tp ${playerName} ~ ~-5 ~`)
       persistentData.firstjoin = true
-    }*/
+    }
 })
-/*PlayerEvents.respawned(event => {
+PlayerEvents.respawned(event => {
   const { player } = event
   const worldSpawn = player.level.getSharedSpawnPos()
   const playerName = player.name.string
     if(player.block.pos.toShortString() == worldSpawn.offset(0,7,0).toShortString()) {//better way of doing this?
       player.runCommandSilent(`tp ${playerName} ~ ~-6 ~`)
     }
-})*/
+})
